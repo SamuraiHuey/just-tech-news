2a3762a5237561c52fad54838828d76fcf13011e
Will B <tryarc@comcast.net>
2022-01-25 20:03:18 -0500

    added login/logout

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSBD_URL){
-  sequelize = new Sequelize(process.env.JAWSBD_URL);
+if (process.env.JAWSDB_URL){
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+ sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
